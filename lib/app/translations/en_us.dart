9eb6a276853d7d23ee85a6911d4054d24240aca2
vivek2730 <vivek.p@digipay.guru>
2025-07-03 11:54:11 +0530

    Translations Done

---
+++ lib/app/translations/en_us.dart
@@ -0,0 +1,9 @@
+final Map<String, String> enUS = {
+  'app_title': 'Shopping App',
+  'loading': 'Loading...',
+  'cart_items': 'items in cart',
+  'drag_here': 'Drag items here to add to cart',
+  'drop_here': 'Drop here to add to cart',
+  'error_fetching_data': 'Failed to fetch data',
+  'no_data': 'No data available',
+};
---
+++ lib/app/translations/hi_in.dart
@@ -0,0 +1,9 @@
+final Map<String, String> hiIN = {
+  'app_title': 'खरीदारी ऐप',
+  'loading': 'लोड हो रहा है...',
+  'cart_items': 'कार्ट में आइटम्स',
+  'drag_here': 'आइटम को खींचकर यहाँ लाएं',
+  'drop_here': 'आइटम यहाँ छोड़ें',
+  'error_fetching_data': 'डेटा लाने में विफल',
+  'no_data': 'कोई डेटा उपलब्ध नहीं',
+};
